9e654d9021896b64690718be380120277f418ca8
MohamedMoubarakHussain <mohmedmoberk64@gmail.com>
2023-07-16 04:39:28 +0300

    Creating The project structure

---
+++ src/test/java/com/myproject/task_managment_system/task_managment_system/TaskManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.task_managment_system.task_managment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
